72806585bd65a4483c9bd6fb25f52a379a017562
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2026-03-17 10:18:12 -0600

    complete unit 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TopNavbar} from './components/top-navbar/top-navbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopNavbar],
+  standalone:true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('hr-timesheet');
+  protected title = 'hr-timesheet';
 }
--- src/app/components/top-navbar/top-navbar.ts
+++ src/app/components/top-navbar/top-navbar.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../modules/material-module';
 
 @Component({
   selector: 'app-top-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    MaterialModule
+  ],
   templateUrl: './top-navbar.html',
-  styleUrl: './top-navbar.scss',
+  styleUrls: ['./top-navbar.scss']
 })
-export class TopNavbar {}
+export class TopNavbar {
+
+}
