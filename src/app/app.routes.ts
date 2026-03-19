b738882274ed6e3fbe1f40af9635243b1cb4de7e
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2026-03-19 10:28:22 -0600

    complete unit 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Departments} from './components/departments/departments'
+import { Timesheet } from './components/timesheet/timesheet';
+import { Analytics} from './components/analytics/analytics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: Departments },
+  { path: 'timesheet', component: Timesheet },
+  { path: 'analytics', component: Analytics }
+];
--- src/app/components/top-navbar/top-navbar.ts
+++ src/app/components/top-navbar/top-navbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../modules/material-module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
   imports: [
-    MaterialModule
+    MaterialModule,
+    RouterLink
   ],
   templateUrl: './top-navbar.html',
   styleUrls: ['./top-navbar.scss']
