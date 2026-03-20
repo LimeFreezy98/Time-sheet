9e0a02e2496205d71d1886ac36a215cb7251cd76
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2026-03-20 08:08:22 -0600

    complete unit 6

--- src/app/components/departments/departments.ts
+++ src/app/components/departments/departments.ts
@@ -3,6 +3,8 @@ import { DepartmentsService } from '../../services/departments';
 import { Department } from '../../interfaces/department';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../modules/material-module.js';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-departments',
@@ -14,9 +16,17 @@ import { MaterialModule } from '../../modules/material-module.js';
 export class Departments implements OnInit {
   departments!: Department[];
 
-  constructor(private departmentsService: DepartmentsService) {}
+  constructor(private departmentsService: DepartmentsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
   }
+
+   
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['/timesheet', {id: departmentId}]);
+  }
+
 }
